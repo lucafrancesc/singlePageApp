47171da5e3b360ba186c7ce9be629fffaf4e4b1c
Luca <eto.lucafrancesco@gmail.com>
2018-12-12 09:30:24 +0000

    event listener added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-(function(exports) {
+// (function(exports) {
+//
+  // function display() {
+  //   var x = document.getElementById("text").value;
+  //   document.getElementById("display").innerHTML = showText(x);
+  // }
+//
+//   exports.display = display
+//
+// })(this);
 
-  function display() {
-      var x = document.getElementById("text").value;
-      document.getElementById("display").innerHTML = showText(x);
-  }
-  exports.display = display
-})(this);
+function display() {
+  var x = document.getElementById("text").value;
+  document.getElementById("display").innerHTML = showText(x);
+}
+
+document.getElementById("btn").addEventListener("click", display);
